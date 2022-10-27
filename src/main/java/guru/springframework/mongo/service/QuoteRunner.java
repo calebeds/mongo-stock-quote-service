459f3461a56093d89fffdd6b502aed745ed78882
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-10-27 11:00:20 -0300

    add quote runner

---
+++ src/main/java/guru/springframework/mongo/service/QuoteRunner.java
@@ -0,0 +1,33 @@
+package guru.springframework.mongo.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class QuoteRunner implements CommandLineRunner {
+
+    private final QuoteGeneratorService quoteGeneratorService;
+    private final QuoteHistoryService quoteHistoryService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        quoteGeneratorService.fetchQuoteStream(Duration.ofMillis(100L))
+                .take(50)
+                .log("Got Quote:")
+                .flatMap(quoteHistoryService::saveQuoteToMongo)
+                .subscribe(savedQuote -> {
+                    log.debug("Saved Quote: " + savedQuote);
+                }, throwable -> {
+                    // handler error here...
+                    log.error("Some error: ", throwable);
+                }, () -> {
+                    log.debug("All done!!");
+                });
+    }
+}
